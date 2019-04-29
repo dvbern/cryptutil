bc755bf7f791db1e8cffebc34541f239eaa2d896
John Bergqvist <31512273+JohnLBergqvist@users.noreply.github.com>
2019-04-29 13:21:04 +0100

    Add unit tests for cryptutil.readers These tests were written using Diffblue Cover.

---
+++ src/test/java/ch/dvbern/lib/cryptutil/readers/IdentityKeyReaderTest.java
@@ -0,0 +1,25 @@
+package ch.dvbern.lib.cryptutil.readers;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.PrivateKey;
+import java.io.IOException;
+import java.net.URL;
+import org.junit.jupiter.api.Test;
+
+import static ch.dvbern.lib.cryptutil.TestingUtil.resourceURL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IdentityKeyReaderTest {
+
+  @Test
+  public void test_readPrivateKey() throws IOException {
+    final URL privateKeyURL = resourceURL("signing/testkey-nopass-pkcs8.pem");
+    final RSAPrivateKey rsaKey = new PKCS8PEMKeyReader(privateKeyURL.openStream(), null).readPrivateKey();
+    
+    final PrivateKey key = new IdentityKeyReader(rsaKey).readPrivateKey();
+    assertEquals("RSA", key.getAlgorithm());
+    assertEquals("PKCS#8", key.getFormat());
+  }
+}
+
---
+++ src/test/java/ch/dvbern/lib/cryptutil/readers/PKCS8PEMKeyReaderTest.java
@@ -0,0 +1,32 @@
+package ch.dvbern.lib.cryptutil.readers;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.io.IOException;
+import java.net.URL;
+import org.junit.jupiter.api.Test;
+
+import static ch.dvbern.lib.cryptutil.TestingUtil.resourceURL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PKCS8PEMKeyReaderTest {
+
+  @Test
+  public void test_readPrivateKey() throws IOException {
+    final URL privateKeyURL = resourceURL("signing/testkey-nopass-pkcs8.pem");
+
+    final RSAPrivateKey key = new PKCS8PEMKeyReader(privateKeyURL.openStream(), null).readPrivateKey();
+    assertEquals("RSA", key.getAlgorithm());
+    assertEquals("PKCS#8", key.getFormat());
+  }
+
+  @Test
+  public void test_readPrivateKeyInvalidPassword() throws IOException {
+    final URL privateKeyURL = resourceURL("signing/testkey-nopass-pkcs8.pem");
+    
+    final ReaderException thrown = 
+      assertThrows(ReaderException.class, () -> new PKCS8PEMKeyReader(privateKeyURL.openStream(), "foo").readPrivateKey());
+    assertEquals("Could not read PKCS8EncodedPEM", thrown.getMessage());
+  }
+}
+
