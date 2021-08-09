9b81d6309e1ada8003d5e1ca2ee566834a79408c
Fabio Heer <fabio.heer@dvbern.ch>
2021-08-09 18:35:07 +0200

    fix invalid javadoc

--- src/main/java/ch/dvbern/lib/cryptutil/annotations/NonNull.java
+++ src/main/java/ch/dvbern/lib/cryptutil/annotations/NonNull.java
@@ -25,7 +25,7 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
  * Marker annotation: passed instances <strong>must be != null</strong>
  *
  * We roll our own because we do not want additional dependencies!
- * Detailed semantics, see <a href="https://checkerframework.org/manual/#nullness-checker>Checker Framework</a>
+ * Detailed semantics, see <a href="https://checkerframework.org/manual/#nullness-checker">Checker Framework</a>
  */
 @Target({ ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.TYPE,
 		ElementType.TYPE_PARAMETER, ElementType.TYPE_USE })
--- src/main/java/ch/dvbern/lib/cryptutil/annotations/Nullable.java
+++ src/main/java/ch/dvbern/lib/cryptutil/annotations/Nullable.java
@@ -22,7 +22,7 @@ import java.lang.annotation.Target;
  * Marker annotation: instances <strong>may be null</strong>.
  *
  * We roll our own because we do not want additional dependencies!
- * Detailed semantics, see <a href="https://checkerframework.org/manual/#nullness-checker>Checker Framework</a>
+ * Detailed semantics, see <a href="https://checkerframework.org/manual/#nullness-checker">Checker Framework</a>
  */
 @Target({ ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.TYPE,
 		ElementType.TYPE_PARAMETER, ElementType.TYPE_USE })
